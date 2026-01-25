88dc77b74984861ef7f8c78efa2f53cd2b23936f
Ashish Premchand Vishwakarma <ashishvish0105@gmail.com>
2026-01-25 17:23:21 +0530

    auth pages structurer comeplete

---
+++ src/app/Auth/components/otpverification/otpverification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-otpverification',
+  standalone: true,
+  imports: [],
+  templateUrl: './otpverification.component.html',
+  styleUrl: './otpverification.component.scss'
+})
+export class OTPVerificationComponent {
+
+}
---
+++ src/app/Auth/main-auth/main-auth.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-auth',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-auth.component.html',
+  styleUrl: './main-auth.component.scss'
+})
+export class MainAuthComponent {
+
+}
