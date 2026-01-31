897e9510004be703d63f38abb97c24a8d77f4a75
Ashish Premchand Vishwakarma <ashishvish0105@gmail.com>
2026-01-31 13:48:38 +0530

    new layout implementing

--- src/app/Auth/main-auth/main-auth.component.ts
+++ src/app/Auth/main-auth/main-auth.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from "../components/login/login.component";
 
 @Component({
   selector: 'app-main-auth',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent],
   templateUrl: './main-auth.component.html',
   styleUrl: './main-auth.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MainAuthComponent } from './Auth/main-auth/main-auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainAuthComponent, title: 'login page' },
+];
